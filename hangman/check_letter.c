7e03187a6d279061ca803fb52b21d222d4ef3486
Kb-Mash <karabo.kmashile@gmail.com>
2023-05-01 22:59:15 +0200

    check if letter is a match

---
+++ hangman/check_letter.c
@@ -0,0 +1,23 @@
+#include "hangman.h"
+
+/**
+ * checkletter - checks if player's letter matches a letter in word
+ * @letter: player's input
+ * @word: random word
+ * @len: length of random word
+ * Return: true if there's a match, else false
+*/
+bool checkletter(char* guessword, char letter, char* word, int len)
+{
+    int i;
+
+    for (i = 0; i < len; i++)
+    {
+        if (word[i] == letter)
+        {
+            guessword[i] = letter;
+            return (true);
+        }
+    }
+    return (false);
+}
