93ea0a7db430e0282413b02d0762a125a98e2e80
Kb-Mash <karabo.kmashile@gmail.com>
2023-05-01 22:59:37 +0200

    game loop

---
+++ hangman/hangman.h
@@ -0,0 +1,17 @@
+#ifndef HANGMAN_H
+#define HANGMAN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <stdbool.h>
+
+/* function prototypes */
+char* randomword(char* words[], int size);
+void init_word(char** guessword, int len);
+bool checkletter(char* guessword, char letter, char* word, int len);
+void hangman(char* word, char* guessword, int len);
+void printword(char* guessword);
+
+#endif
