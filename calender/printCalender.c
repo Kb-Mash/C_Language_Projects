6d95f82ba97e5cb29c359dbe3b824eb9ebb31ee8
Kb-Mash <karabo.kmashile@gmail.com>
2023-05-06 14:35:55 +0200

    prints the calender

---
+++ calender/printCalender.c
@@ -0,0 +1,35 @@
+#include "calender.h"
+
+void print_calender(int month, int year)
+{
+	int num_days, weekday, spaces, date = 1;
+
+	printf("\n Calender - %d\n", year);
+
+	weekday = day_of_week(date, month, year);
+	num_days = get_days(month, year);
+
+	printf("\n ------------%s------------\n", get_month(month));
+	printf(" Sat Sun Mon Tue Wed Thu Fri\n");
+
+	for (spaces = 0; spaces < weekday; spaces++)
+	{
+		printf("    ");
+	}
+
+	for(; date <= num_days; date++)
+	{
+		printf("%4d", date);
+
+		if (++spaces > 6)
+		{
+			spaces = 0;
+			printf("\n");
+		}
+	}
+	if (spaces)
+		printf("\n");
+	weekday = spaces;
+
+	printf("\n");
+}
